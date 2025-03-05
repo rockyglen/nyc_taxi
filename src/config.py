5236034077bba8ca3c9b2dbca005c131189a0122
rockyglen <rockyglen86@gmail.com>
2025-03-05 13:21:34 -0500

    8th Commit

--- src/config.py
+++ src/config.py
@@ -1,15 +1,27 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Define directories
 PARENT_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = PARENT_DIR / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
 TRANSFORMED_DATA_DIR = DATA_DIR / "transformed"
+MODELS_DIR = PARENT_DIR / "models"
 
-for directory in [DATA_DIR, RAW_DATA_DIR, PROCESSED_DATA_DIR, TRANSFORMED_DATA_DIR]:
+# Create directories if they don't exist
+for directory in [
+    DATA_DIR,
+    RAW_DATA_DIR,
+    PROCESSED_DATA_DIR,
+    TRANSFORMED_DATA_DIR,
+    MODELS_DIR,
+]:
     directory.mkdir(parents=True, exist_ok=True)
-
 
 
 HOPSWORKS_API_KEY = os.getenv("HOPSWORKS_API_KEY")
